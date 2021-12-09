2251a66690c11bb9f22188d459b64542643ad2ed
Sokol <77862649+OfficialSokol@users.noreply.github.com>
2021-12-09 21:11:15 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -38,6 +38,8 @@ AddEventHandler('sokol:adrenalin:rokniAdrenalin', function()
         tajmer = 0
 		ESX.UI.Menu.CloseAll()
 
+		TriggerServerEvent('sokol:adrenalin:oduzmi')
+        DoScreenFadeOut(1000)
         local lib, anim = 'anim@mp_player_intcelebrationmale@face_palm', 'face_palm'
         ESX.Streaming.RequestAnimDict(lib, function()
             TaskPlayAnim(playerPed, lib, anim, 8.0, -8.0, -1, 32, 0, false, false, false)
@@ -47,8 +49,8 @@ AddEventHandler('sokol:adrenalin:rokniAdrenalin', function()
                 DisableAllControlActions(0)
             end
         end)
-		TriggerServerEvent('sokol:adrenalin:oduzmi')
-        DoScreenFadeOut(1000)
+        Citizen.Wait(1000)
+        DoScreenFadeIn(1000)
 		repeat
 			Citizen.Wait(1000)
 			RestorePlayerStamina(PlayerId(), 1.0)
@@ -57,8 +59,9 @@ AddEventHandler('sokol:adrenalin:rokniAdrenalin', function()
         DoScreenFadeOut(1000)
         Citizen.Wait(1000)
         DoScreenFadeIn(1000)
+        ESX.ShowNotification('Adrenalin vas ~y~popusta~w~.')
 		podAdrenalinom = false
 	else
-		ESX.ShowNotification('Vec ste pod uticajem adrenalina.')
+		ESX.ShowNotification('Vec ste pod uticajem ~y~adrenalina~w~.')
 	end
 end)
